d772b69d56441328e16be3970942081f55b9b93a
David Castanheira <david.castanheira@aluno.faculdadeimpacta.com.br>
2024-02-24 01:08:03 -0300

    Organização da estrutura do projeto e criação da conexão com o banco de dados MySQL

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Configuração do banco
+
+# api.secret_key = 'you secret key' (manter comentado temporariamente)
+
+# app.config["MYSQL_USER"] = "user_db"
+# app.config["MYSQL_PASSWORD"] = "password"
+# app.config["MYSQL_DB"] = "database"
+
