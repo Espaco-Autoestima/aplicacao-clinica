ae5aa0b054a2dd747cfd3048ae2652d11a9754b3
David Castanheira <david.castanheira@aluno.faculdadeimpacta.com.br>
2024-02-24 15:04:24 -0300

    Inclusão da configuração e conexão com o banco de dados

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_mysqldb import MySQL 
 
 app = Flask(__name__)
 
-# Configuração do banco
+# Configuração e conexão do banco
 
 # api.secret_key = 'you secret key' (manter comentado temporariamente)
 
-# app.config["MYSQL_USER"] = "user_db"
-# app.config["MYSQL_PASSWORD"] = "password"
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
 # app.config["MYSQL_DB"] = "database"
+
+mysql = MySQL(app)
 
