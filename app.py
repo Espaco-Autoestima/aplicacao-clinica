02ce9a478e8b24d94fa42b9282d012b470a09edf
David Castanheira <david.castanheira@aluno.faculdadeimpacta.com.br>
2024-02-28 20:23:42 -0300

    Atualização no arquivo referente à nova instalação para conexão com o banco de dados

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-import os
 from flask import Flask, render_template, json, request
-from flaskext.mysql import MySQL 
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 # Configuração e conexão do banco
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'E$p@c02024!'
-# Criar database
 app.config['MYSQL_DATABASE_DB'] = ''
 app.config['MYSQL_DATABASE_HOST'] = ''
 
-mysql.init_app(app)
+mysql = MySQL(app)
 
 # def cadastro():
 #     return render_template('cadastro.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
