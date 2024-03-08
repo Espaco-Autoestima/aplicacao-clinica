e117e299046b9afe9655d45523dc15c1849fa242
David Castanheira <david.castanheira@aluno.ifsp.edu.br>
2024-03-08 13:35:34 -0300

    Inclusão de campos nas tabelas clientes e profissionais

--- clientes.py
+++ clientes.py
@@ -4,30 +4,33 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # Configuração e conexão do banco (a conexão não está funcionando)
+# app.config['MYSQL_USER'] = "dev"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = "E$p@c02024!"
 app.config['MYSQL_DB'] = "espacoautoestima"
-app.config['MYSQL_HOST'] = "172.17.0.2"
+app.config['MYSQL_HOST'] = "localhost"
 
 mysql = MySQL(app)
 
-@app.route('/agendamento')
+@app.route('/')
 def index():
-    return render_template('agendamento.html')
+    return "Seja bem-vindo!"
 
-# @app.route('/cadastroClientes', methods=['POST'])
-# def adicionarCliente():
-#     return render_template('cadastro-clientes.html')
+@app.route('/iniciar')
+def iniciar():
+    return render_template('cadastro-clientes.html')
 
-# Criar rotas das operações básicas do banco (CRUD)
-@app.route('/clientes/cadastrar', methods=['POST'])
+# Criar rotas das operações básicas do banco (CRUD) de clientes, exceto DELETE
+@app.route('/cadastrarCliente', methods=['POST'])
 def adicionarCliente():
-    if request.method == 'POST' and 'nome' in request.form and 'telefone' in request.form:
+    if request.method == 'POST' and 'nome' in request.form and 'telefone' in request.form and 'email' in request.form and 'endereco' in request.form:
         nome = request.form['nome']
         telefone = request.form['telefone']
+        email = request.form['email']
+        endereco = request.form['endereco']
 
         cursor = mysql.connection.cursor()
-        cursor.execute("INSERT INTO clientes(nome, telefone) VALUES (%s, %s)", (nome, telefone))
+        cursor.execute("INSERT INTO clientes(nome, telefone, email, endereco) VALUES (%s, %s, %s, %s)", (nome, telefone, email, endereco))
         mysql.connection.commit()
         return redirect(url_for('success'))
     return render_template('clientes.html')
@@ -51,24 +54,35 @@ def atualizarCliente(id):
         return redirect(url_for('success'))
     return render_template('clientes.html')
 
-@app.route('/clientes/deletar/<int:id>')
-def deletarCliente():
-    cursor = mysql.connection.cursor()
-    cursor.execute("DELETE FROM clientes WHERE id=%s", (id))
-    mysql.connection.commit()
-    return render_template('clientes.html')
+# Criar rotas das operações básicas do banco (CRUD) de profissionais, exceto DELETE
+# @app.route('/cadastrarProfissionais')
+# def cadastrarProfissional():
+#     if request.method == 'POST' and 'nome' in request.form and 'especialidade' in request.form:
+#         nome = request.form['nome']
+#         especialidade = request.form['especializacao']
+# Adicionar telefone e endereço
 
-@app.route('/profissionais/cadastrar')
-def cadastrarProfissional():
-    if request.method == 'POST' and 'nome' in request.form and 'especialidade' in request.form:
-        nome = request.form['nome']
-        especialidade = request.form['especializacao']
+#         cursor = mysql.connection.cursor()
+#         cursor.execute("INSERT INTO profissionais(nome, especialidade) VALUES(%s, %s)", (nome, especialidade))
+#         mysql.connection.commit()
+#         return redirect(url_for('success'))
+#     return render_template('profissionais.html')
 
-        cursor = mysql.connection.cursor()
-        cursor.execute("INSERT INTO profissionais(nome, especialidade) VALUES(%s, %s)", (nome, especialidade))
-        mysql.connection.commit()
-        return redirect(url_for('success'))
-    return render_template('profissionais.html')
+# Criar rotas das operações básicas do banco (CRUD) de agendamentos/consultas, exceto DELETE
+# @app.route('/agendamento', methods=['POST', 'GET'])
+# def agendamento():
+#     if request.method == 'POST' and 'nome' in request.form and 'telefone' in request.form: #and 'nomep' in request.form and 'sessao' in request.form and 'horario' in request.form:
+#         nome = request.form['nome']
+#         #nomep = request.form['nomep']
+#         telefone = request.form['telefone']
+#         sessao = request.form['sessao']
+#         # alterar campo na tabela do banco
+#         horario = request.form['horario']
+        
+#         cursor = mysql.connection.cursor()
+#         cursor.execute("INSERT INTO consulta(nome, telefone, sessao, horario) VALUES (%s, %s, %s, %s)", (nome, telefone, sessao, horario))
+#         mysql.connection.commit()
+#     return render_template('agendamento.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
